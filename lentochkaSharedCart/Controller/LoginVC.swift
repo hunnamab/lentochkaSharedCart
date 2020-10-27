250bb0ad2f7dbf3ec3b1f267ad7f9bafcd0d44d3
tpepperm <tkarpukova@gmail.com>
2020-10-28 00:38:26 +0300

    feat: create LoginVC programmatically, remove SceneDelegate for iOS 12 compatibility, edit AppDelegate to launch root view controller from code

--- lentochkaSharedCart/Controller/LoginVC.swift
+++ lentochkaSharedCart/Controller/LoginVC.swift
@@ -9,17 +9,75 @@ import UIKit
 
 class LoginVC: UIViewController {
 
-    @IBOutlet weak var loginTextField: UITextField!
-    @IBOutlet weak var passwordTextField: UITextField!
-    @IBOutlet weak var loginButton: CustomButton!
+    var loginTextField = UITextField()
+    var passwordTextField = UITextField()
+    var loginButton = UIButton()
+    var forgotPasswordLabel = UILabel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setUI()
+        setConstraints()
     }
     
-    @IBAction func loginButtonWasPressed(_ sender: Any) {
-        performSegue(withIdentifier: "TabBarSegue", sender: self)
+    private func setUI() {
+        view.backgroundColor = .white
+        
+        loginTextField.placeholder = "Логин"
+        loginTextField.textAlignment = .center
+        loginTextField.textColor = .black
+        loginTextField.font = UIFont.systemFont(ofSize: 20, weight: .regular)
+        loginTextField.layer.borderColor = UIColor.lightGray.cgColor
+        loginTextField.layer.borderWidth = 1
+        loginTextField.layer.cornerRadius = 8
+        loginTextField.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        
+        passwordTextField.placeholder = "Пароль"
+        passwordTextField.textAlignment = .center
+        passwordTextField.textColor = .black
+        passwordTextField.font = UIFont.systemFont(ofSize: 20, weight: .regular)
+        passwordTextField.layer.borderColor = UIColor.lightGray.cgColor
+        passwordTextField.layer.borderWidth = 1
+        passwordTextField.layer.cornerRadius = 8
+        passwordTextField.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        
+        loginButton.setTitle("Войти", for: .normal)
+        loginButton.setTitleColor(.purple, for: .normal)
+        loginButton.titleLabel?.font = UIFont.systemFont(ofSize: 24, weight: .medium)
+        
+        forgotPasswordLabel.text = "Забыли пароль?"
+        forgotPasswordLabel.font = UIFont.systemFont(ofSize: 20, weight: .regular)
+        forgotPasswordLabel.textColor = .black
+    }
+    
+    private func setConstraints() {
+        let loginStackView = UIStackView(arrangedSubviews: [loginTextField, passwordTextField])
+        loginStackView.axis = .vertical
+        loginStackView.distribution = .equalSpacing
+        loginStackView.spacing = 20
+        loginStackView.alignment = .fill
+        
+        let buttonStackView = UIStackView(arrangedSubviews: [loginButton, forgotPasswordLabel])
+        buttonStackView.axis = .vertical
+        buttonStackView.distribution = .fillEqually
+        buttonStackView.spacing = 50
+        buttonStackView.alignment = .center
+        
+        let stackView = UIStackView(arrangedSubviews: [loginStackView, buttonStackView])
+        stackView.axis = .vertical
+        stackView.distribution = .equalSpacing
+        stackView.alignment = .fill
+        
+        view.addSubview(stackView)
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            stackView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.7),
+            stackView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.35)
+        ])
     }
 }
 
